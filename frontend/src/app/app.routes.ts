5426268e6fb80f1ac8382bd18d570d69e014df7f
AntonioF842 <antoemman@hotmail.com>
2025-11-22 15:54:54 -0600

    Página para explorar

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { Login } from './pages/login/login';
 import { Register } from './pages/register/register';
 import { Dashboard } from './pages/dashboard/dashboard';
+import { Explore } from './pages/explore/explore';
 import { authGuard, guestGuard } from './shared/guards/auth.guard';
 
 export const routes: Routes = [
@@ -14,6 +15,11 @@ export const routes: Routes = [
   {
     path: 'dashboard',
     component: Dashboard
+  },
+
+  {
+    path: 'explore',
+    component: Explore
   },
 
   {
